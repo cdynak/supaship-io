cd1c27c5882cf426613e2006aeca82d0f446ac2d
Cezary Dynak <cezary.dynak@gmail.com>
2022-12-19 07:55:27 +0100

    Customize components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-import { createBrowserRouter } from 'react-router-dom'
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
 import MessageBoard from './MessageBoard'
 import AllPosts from './AllPosts'
 import PostView from './PostView'
 import Welcome from './Welcome'
+import NavBar from './NavBar'
 
 const router = createBrowserRouter([
   {
@@ -35,36 +34,14 @@ const router = createBrowserRouter([
 ])
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="text-5xl text-blue-500">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
 
 function Layout() {
-  return <h2>Layout</h2>;
+  return <>
+    <NavBar />
+    <Outlet />
+  </>;
 }
